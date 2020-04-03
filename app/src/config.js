7d5b212539ec742675c7eb86b6806c08c96883a6
Clayton Poitevin <Clayton@idealsoft.com.br>
2020-04-03 11:19:21 -0300

    variaveis do react devem começar com REACT_APP

--- app/src/config.js
+++ app/src/config.js
@@ -2,6 +2,6 @@ var config = {};
 
 config.api = {};
 
-config.api.url = process.env.API_URL || 'http://localhost:3010';
+config.api.url = process.env.REACT_APP_API_URL || 'http://localhost:3010';
 
 module.exports = config;
